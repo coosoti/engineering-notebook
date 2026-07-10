bc2db371d0fb085933829a531a420621eab89291
coosoti <charlesosoti@gmail.com>
2026-07-10 18:44:42 +0300

    chores: add fix to authentic:final

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,12 @@
 import { handlers } from "@/lib/auth"
+import { NextRequest } from "next/server"
 
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
   console.log('SENTRY: Auth API GET request received')
   return handlers.GET(request)
 }
 
-export async function POST(request: Request) {
+export async function POST(request: NextRequest) {
   console.log('SENTRY: Auth API POST request received')
   return handlers.POST(request)
 }
