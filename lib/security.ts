7bb7d4ac41ed4fb843bfcb7dd7dc20cb508717cd
coosoti <charlesosoti@gmail.com>
2026-07-20 17:26:54 +0300

    Chores: Run time errors resolution

--- lib/security.ts
+++ lib/security.ts
@@ -1,11 +1,45 @@
-import DOMPurify from 'isomorphic-dompurify'
+import sanitizeHtmlLib from 'sanitize-html'
 import crypto from 'crypto'
 
 /**
  * Sanitizes HTML content to prevent XSS attacks.
  */
 export function sanitizeHtml(html: string): string {
-  return DOMPurify.sanitize(html)
+  return sanitizeHtmlLib(html, {
+    allowedTags: [
+      'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+      'p', 'br', 'strong', 'em', 'u', 's', 'sub', 'sup',
+      'ul', 'ol', 'li', 'dl', 'dt', 'dd',
+      'blockquote', 'cite', 'q',
+      'code', 'pre', 'kbd', 'samp', 'var',
+      'a', 'img', 'figure', 'figcaption',
+      'table', 'thead', 'tbody', 'tr', 'th', 'td',
+      'div', 'span', 'section', 'article', 'aside',
+      'hr'
+    ],
+    allowedAttributes: {
+      '*': ['class', 'id', 'style'],
+      'a': ['href', 'title', 'target', 'rel'],
+      'img': ['src', 'alt', 'title', 'width', 'height', 'loading'],
+      'code': ['class'],
+      'pre': ['class'],
+      'th': ['scope'],
+      'td': ['colspan', 'rowspan'],
+    },
+    allowedSchemes: ['http', 'https', 'mailto'],
+    allowedClasses: {
+      'code': ['language-*'],
+      'pre': ['language-*'],
+    },
+    transformTags: {
+      'a': (tagName: string, attribs: Record<string, string>) => {
+        if (attribs.href && attribs.href.startsWith('http')) {
+          return { tagName, attribs: { ...attribs, target: '_blank', rel: 'noopener noreferrer' } }
+        }
+        return { tagName, attribs }
+      }
+    }
+  })
 }
 
 /**
